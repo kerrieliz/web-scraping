f93d5a7a54addf67fb6449c55afddf4632a2f501
Kerrie Gill <kerriegill@gmail.com>
2019-12-02 15:58:45 -0700

    Update

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+import scrape_mars
+
+import pymongo
+
+app = Flask(__name__)
+
+conn = 'mongodb://localhost:27017/mission_to_mars'
+
+client = pymongo.MongoClient(conn)
+
+@app.route("/")
+def index():
+    mars = client.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+@app.route("/scrape")
+def scrape():
+    mars = client.db.mars
+    mars_data = scrape_mars.scrape()
+    mars.update({}, mars_data)
+    return "Scraped!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ scrape_mars.py
@@ -0,0 +1,7 @@
+# Dependencies
+from splinter import Browser
+from bs4 import BeautifulSoup
+import pandas as pd
+from pprint import pprint
+from time import sleep
+
